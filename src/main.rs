95fdbb3bb2f0c08f580709d981ab1f1804f5e0d7
Your Name <Your Name>
2026-06-24 18:22:04 +0200

    added clap functionality, capability to add new entries

--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,237 @@
 use chrono::Local;
+use clap::{Args, Parser};
 use serde::Deserialize;
-use std::env;
-use std::error::Error;
+// use std::env;
+// use std::error::Error;
 use std::fs;
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::Path;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    bp: BlPrArgs,
+
+    /// Generate output
+    #[arg(short, long)]
+    output: bool,
+
+    /// Display status
+    #[arg(short, long)]
+    status: bool,
+}
+
+#[derive(Debug, Args)]
+#[group(multiple = true)]
+struct BlPrArgs {
+    /// Systolic blood pressure, mmHg
+    #[arg(requires_all=["dia","pul"])]
+    sys: Option<f32>,
+
+    /// Diastolic blood pressure, mmHg
+    #[arg(requires_all=["sys","pul"])]
+    dia: Option<f32>,
+
+    /// Heart rate, #pulses/min
+    #[arg(requires_all=["sys","dia"])]
+    pul: Option<f32>,
+}
+impl BlPrArgs {
+    fn check_state(self: &Self) -> BlPrArgState {
+        if self.sys.is_none() && self.dia.is_none() && self.pul.is_none() {
+            return BlPrArgState::Empty;
+        } else if self.sys.is_some() && self.dia.is_some() && self.pul.is_some() {
+            return BlPrArgState::Valid;
+        } else {
+            return BlPrArgState::Invalid;
+        };
+    }
+    fn to_measurement(&self) -> Measurement {
+        match self.check_state() {
+            BlPrArgState::Empty => {
+                panic!("Attempting to create Measurement from empty Blood Pressure args!")
+            }
+            BlPrArgState::Invalid => {
+                panic!("Attempting to create Measurement from invalid Blood Pressure args!")
+            }
+            BlPrArgState::Valid => (),
+        }
+        let sys = self.sys.unwrap();
+        let dia = self.dia.unwrap();
+        let pul = self.pul.unwrap();
+        return Measurement::new(sys, dia, pul);
+    }
+}
+
+#[derive(PartialEq)]
+enum BlPrArgState {
+    Empty,
+    Valid,
+    Invalid,
+}
+
+// ################################################################
 
 #[derive(Debug, Deserialize)]
 struct Measurement {
     date: String,
     time: String,
-    sys: String,
-    dia: String,
-    pul: String,
+    sys: f32,
+    dia: f32,
+    pul: f32,
 }
 impl Measurement {
-    pub fn new_full(
-        &self,
-        date: String,
-        time: String,
-        sys: String,
-        dia: String,
-        pul: String,
-    ) -> Measurement {
-        self.check_parameter(&date, &time, &sys, &dia, &pul);
-        Measurement {
-            date,
-            time,
+    pub fn new(sys: f32, dia: f32, pul: f32) -> Measurement {
+        return Measurement {
+            date: get_date(),
+            time: get_time(),
             sys,
             dia,
             pul,
-        }
+        };
     }
-    pub fn new(&self, sys: String, dia: String, pul: String) -> Measurement {
-        self.new_full(get_date(), get_time(), sys, dia, pul)
-    }
-    fn check_parameter(&self, date: &str, time: &str, sys: &str, dia: &str, pul: &str) {
-        if !date.contains("-") {
-            panic!("Date '{}' not in correct format (YYYY-mm-dd)!", date);
-        }
-        if !time.contains(":") {
-            panic!("Date '{}' not in correct format (HH:MM:SS)!", time);
-        }
-    }
-    pub fn get_measurement(&self) -> (&str, &str, &str) {
-        (&self.sys, &self.dia, &self.pul)
-    }
+
+    // pub fn get_measurement(&self) -> (f32, f32, f32) {
+    //     (self.sys, self.dia, self.pul)
+    // }
 }
 
+// ################################################################
+// ################################################################
 fn main() {
-    let _args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
+    println!("CLI: {:?}\n", cli);
 
-    // let query = &args[1];
-    // let file_path = &args[2];
+    // let bp = &cli.bp;
+    // let show_status = &cli.status;
+    // let generate_output = &cli.output;
 
-    // println!("Searching for {query}");
-    // println!("In file {file_path}");
+    worker_init_csv();
 
-    // let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    worker_bp_add(&cli);
 
-    // println!("With text:\n{contents}");
+    if cli.output {
+        // worker_pdf_output();
+    } else if cli.status {
+        worker_csv_status();
+    };
 
-    setup_execution();
+    return;
 
-    run().unwrap();
+    // run().unwrap();
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
-    // println!("Searching for {data_path_valid}");
-    read_csv_content().expect("Unable to perform 'Read of CSV File'.");
+fn worker_csv_status() {
+    let date_ym = get_date_ym();
+    let entries = read_csv_content().expect("Unable to perform 'Read of CSV File'.");
+    let l = entries.len();
+    let r = if l <= 5 { 0..l } else { (l - 5)..l };
 
-    Ok(())
+    println!("File for '{date_ym}' contains {l} entries.");
+
+    if l > 0 {
+        println!("Latest entries:");
+        for i in r {
+            println!("[{}] {:?}", i + 1, entries[i]);
+        }
+    }
 }
 
-fn setup_execution() {
+fn worker_bp_add(cli: &Cli) {
+    let bp = &cli.bp;
+
+    match bp.check_state() {
+        BlPrArgState::Valid => (),
+        BlPrArgState::Empty => return,
+        BlPrArgState::Invalid => panic!("Invalid Blood Pressure args!"),
+    }
+    let m = bp.to_measurement();
+
+    let path_string = get_file_path_string();
+    let path_file = Path::new(&path_string);
+
+    let fh = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(path_file)
+        .expect(&format!("Unable to open File '{}'.", path_file.display()));
+
+    if let Err(e) = writeln!(
+        &fh,
+        "{},{},{:.1},{:.1},{:.1}",
+        m.date, m.time, m.sys, m.dia, m.pul
+    ) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+}
+
+fn worker_init_csv() {
     check_path().expect("Unable to perform 'Check of /data Directory'.");
     check_file().expect("Unable to perform 'Check of work File'.");
 }
 
 fn read_csv_content() -> Result<Vec<Measurement>, Box<dyn std::error::Error>> {
-    let path_string = format!("./data/{}.csv", get_date_ym());
-    let file_path = Path::new(&path_string);
+    let path_string = get_file_path_string();
+    let path_file = Path::new(&path_string);
 
-    let fh = File::open(file_path)?;
-    let mut rdr = csv::ReaderBuilder::new().delimiter(b';').from_reader(fh);
+    let fh = File::open(path_file)?;
+    let mut rdr = csv::ReaderBuilder::new().delimiter(b',').from_reader(fh);
 
-    let records: Vec<_> = rdr.records().collect();
-    let records_len = records.len();
-    println!("{} contains {} entries:", path_string, records_len);
+    // let records_iter = rdr.deserialize();
 
-    let ret: Vec<Measurement> = Vec::with_capacity(records_len);
+    let records: Vec<Result<Measurement, csv::Error>> = rdr.deserialize().collect();
+    let mut ret: Vec<Measurement> = Vec::with_capacity(records.len());
 
     for result in records {
-        let entry = result?;
-        println!("{:?}", entry);
+        let entry: Measurement = result?;
+        // println!("{:?}", entry);
+        ret.push(entry);
     }
 
     Ok(ret)
 }
 
 fn check_file() -> Result<(), std::io::Error> {
-    let path_string = format!("./data/{}.csv", get_date_ym());
-    let fpath = Path::new(&path_string);
+    let path_string = get_file_path_string();
+    let path_file = Path::new(&path_string);
 
-    if fpath.exists() {
+    if path_file.exists() {
         return Ok(());
     }
-    log_warning(&format!("File '{}' missing.", fpath.display()));
+    log_warning(&format!("File '{}' missing.", path_file.display()));
 
-    let mut fh = match File::create_new(fpath) {
+    let mut fh = match File::create_new(path_file) {
         Ok(f) => {
-            log_message(&format!("File '{}' created.", fpath.display()));
+            log_message(&format!("File '{}' created.", path_file.display()));
             f
         }
         Err(e) => {
-            log_error(&format!("Unable to create File '{}'.", fpath.display()));
+            log_error(&format!(
+                "Unable to create new File '{}'.",
+                path_file.display()
+            ));
             return Err(e);
         }
     };
 
-    writeln!(fh, "Date;Time;SYS;DIA;PUL")?;
+    writeln!(fh, "date,time,sys,dia,pul")?;
 
     Ok(())
 }
 
 fn check_path() -> std::io::Result<()> {
-    let path_string = "./data";
-    let dir_path = Path::new(path_string);
+    let path_string = get_dir_path_string();
+    let path_dir = Path::new(&path_string);
 
-    if dir_path.exists() {
+    if path_dir.exists() {
         return Ok(());
     }
     log_warning(&format!("Directory '{}' missing.", path_string));
 
-    match fs::create_dir(dir_path) {
+    match fs::create_dir(path_dir) {
         Ok(_) => {
             log_message(&format!("Directory '{}' created.", path_string));
             Ok(())
@@ -164,4 +261,11 @@ fn get_date_ym() -> String {
 }
 fn get_time() -> String {
     Local::now().format("%H:%M:%S").to_string()
+}
+
+fn get_file_path_string() -> String {
+    format!("./data/{}.csv", get_date_ym())
+}
+fn get_dir_path_string() -> String {
+    format!("./data")
 }
