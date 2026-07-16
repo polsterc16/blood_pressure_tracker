1c2a6567d1fe66cc74c70c03c229e01a3257f02b
polsterc16 <38907941+polsterc16@users.noreply.github.com>
2026-07-16 17:17:24 +0200

    fn `main`: added use of `BpFileWarden`

--- src/main.rs
+++ src/main.rs
@@ -51,34 +51,35 @@ fn main() {
     let dym_now = DateYearMonth::from_now();
     let file_name_now = dym_now.to_string();
 
-    let fw_data = FileWarden::new_option(Some(dir_data_str), Some(&file_name_now), None);
-    let fw_output = FileWarden::new_option(Some(dir_output_str), Some(&file_name_now), None);
-    fw_data.check_create_directory();
-    fw_output.check_create_directory();
+    let mut fw_bp = BpFileWarden::new(&file_name_now, dir_data_str, dir_output_str);
+    fw_bp.csv_data.check_create_directory();
+    fw_bp.json_output.check_create_directory();
 
-    let mut csv_worker = FileWardenCsv::from_file_warden(&fw_data);
-    csv_worker.check_file().unwrap();
+    // let csv_data_worker = &mut fw_bp.csv_data;
+    // let json_output_worker = &mut fw_bp.json_output;
+
+    fw_bp.csv_data.check_file().unwrap();
 
     // let json_worker = FileWardenJson::new_option(Some("output"), None);
     // let x = json_worker.get_dir_content();
     // println!("Content: {:?}", x);
 
     if let Some(bp) = cli.add {
-        worker_bp_add(&mut csv_worker, &bp);
+        worker_bp_add(&mut fw_bp, &bp);
     }
 
     if cli.rebuild || cli.status || cli.output {
         // let csv_collection = read_csv_content().expect("Unable to perform 'Read of CSV File'.");
-        let csv_collection = csv_worker.get_csv_content().unwrap();
+        let csv_collection = fw_bp.csv_data.get_csv_content().unwrap();
 
         if cli.rebuild {
-            worker_csv_rebuild(&mut csv_worker, &csv_collection);
+            worker_csv_rebuild(&mut fw_bp, &csv_collection);
         }
         if cli.output {
-            worker_output(&csv_worker, &csv_collection);
+            worker_output(&mut fw_bp, &csv_collection);
         }
         if cli.status {
-            worker_csv_status(&csv_worker, &csv_collection);
+            worker_csv_status(&mut fw_bp, &csv_collection);
         }
     }
 
@@ -86,13 +87,14 @@ fn main() {
 }
 
 /// Will read the CSV file, sort measurements and overwrite the file
-fn worker_output(csv_worker: &FileWardenCsv, csv_collection: &CollectionCsv) {
+fn worker_output(fw_bp: &mut BpFileWarden, csv_collection: &CollectionCsv) {
     let coll_m2 = csv_collection.to_coll_m2(2);
 
     let coll_month = CollectionMonth::from_coll_m2_consume(coll_m2);
 
-    let mut out_month = OutputMonth::new(&coll_month);
-    out_month.set_name(&csv_worker.get_file_name());
+    let mut out_month = OutputMonth::new(&coll_month, &fw_bp.json_output);
+    out_month.set_name(&fw_bp.csv_data.get_file_name());
+
     // println!("{out_month:?}");
 
     // println!("{coll_month:?}");
@@ -105,9 +107,10 @@ fn worker_output(csv_worker: &FileWardenCsv, csv_collection: &CollectionCsv) {
 }
 
 /// Will read the CSV file, sort measurements and overwrite the file
-fn worker_csv_rebuild(csv_worker: &mut FileWardenCsv, csv_collection: &CollectionCsv) {
+fn worker_csv_rebuild(fw_bp: &mut BpFileWarden, csv_collection: &CollectionCsv) {
+    let fw_csv_data = &mut fw_bp.csv_data;
     // Open CSV File and reset content
-    let fh_csv = csv_worker.file_open(&FileOpenMode::Write).unwrap();
+    let fh_csv = fw_csv_data.file_open(&FileOpenMode::Write).unwrap();
 
     for entry in csv_collection.get_ref() {
         writeln!(&fh_csv, "{}", entry)
@@ -119,9 +122,10 @@ fn worker_csv_rebuild(csv_worker: &mut FileWardenCsv, csv_collection: &Collectio
     fh_csv.sync_all().context("Unable to save File .").unwrap();
 }
 
-fn worker_csv_status(csv_worker: &FileWardenCsv, csv_collection: &CollectionCsv) {
+fn worker_csv_status(fw_bp: &mut BpFileWarden, csv_collection: &CollectionCsv) {
+    let fw_csv_data = &mut fw_bp.csv_data;
     let csv_entries = csv_collection.get_ref();
-    let date_ym = csv_worker.get_file_name();
+    let date_ym = fw_csv_data.get_file_name();
 
     let csv_len = csv_entries.len();
     println!("File for '{date_ym}' contains {csv_len} entries.");
@@ -140,7 +144,8 @@ fn worker_csv_status(csv_worker: &FileWardenCsv, csv_collection: &CollectionCsv)
     }
 }
 
-fn worker_bp_add(csv_worker: &mut FileWardenCsv, bp: &Vec<f32>) {
+fn worker_bp_add(fw_bp: &mut BpFileWarden, bp: &Vec<f32>) {
+    let fw_csv_data = &mut fw_bp.csv_data;
     let sys = bp[0];
     let dia = bp[1];
     let pul = bp[2];
@@ -148,7 +153,7 @@ fn worker_bp_add(csv_worker: &mut FileWardenCsv, bp: &Vec<f32>) {
     let measurement = MeasCsv::new(sys, dia, pul);
 
     // Open CSV file in 'append' mode
-    let fh_csv = csv_worker.file_open(&FileOpenMode::Append).unwrap();
+    let fh_csv = fw_csv_data.file_open(&FileOpenMode::Append).unwrap();
 
     // Append entry to CSV file
     writeln!(&fh_csv, "{}", measurement)
